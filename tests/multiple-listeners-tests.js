22f5f34b040b4a04ede0756efb869afebf6d2982
Michał Budzyński <michal@virtualdesign.pl>
2015-07-31 01:01:00 +0100

    :suspension_railway: typo fix

--- tests/multiple-listeners-tests.js
+++ tests/multiple-listeners-tests.js
@@ -1,11 +1,12 @@
 'use strict';
 
-/* global apress, module, test, ok*/
+/* global apress, module, test, ok */
 
 module('Apress multiple callbacks to simple routing module');
 
 test('should call multiple callbacks attached to the same route',
-function() {
+function(assert) {
+  var done = assert.async();
   var status1 = false;
   var status2 = false;
   var route = '/foo';
@@ -21,6 +22,34 @@ function() {
   window.location.hash = '#!' + route;
   apress.hashTest();
 
-  ok(status1, 'first listener fired');
+  // something was wrong but async test resolves it
+  setTimeout(function(){
+    ok(status1, 'first listener fired');
+    ok(status2, 'second listener fired');
+    done();
+  }, 0);
+});
+
+test('should remove just one callback if specified in the remove method',
+function() {
+  var status1 = false;
+  var status2 = false;
+  var route = '/foo';
+  var cb1 = function(){
+    status1 = true;
+  };
+  var cb2 = function(){
+    status2 = true;
+  };
+
+  apress.addRoute(route, cb1);
+  apress.addRoute(route, cb2);
+
+  apress.removeRoute(route, cb1);
+
+  window.location.hash = '#!' + route;
+  apress.hashTest();
+
+  ok(!status1, 'first listener doesn\'t fire');
   ok(status2, 'second listener fired');
 });
